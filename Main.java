75a96f28c5bdd76b479df0c6a79e8b710b42bdee
yanakmaev <sve90ufa@gmail.com>
2016-11-05 14:03:43 +0500

    Обработчики

--- Main.java
+++ Main.java
@@ -46,4 +46,32 @@ public class Main extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextPane1 = new javax.swing.JTextPane();
         jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
         
+        //Указываем интерпретатору, что при зиакрытии окна
+        //нужно выйти из приложения
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        
+        //Устанавливаем Title фрейма
+        setTitle("Текстовычислятор");
+        
+        //Устанавливаем необходимые формы на панеле
+        jScrollPane1.setViewportView(jTextPane1);
+
+        jLabel1.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        jLabel1.setText("Вставьте или введите текст");
+
+        jButton1.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        jButton1.setText("Рассчитать");
+        //Прописываем, какой нужно использовать метод при нажатии на кнопку
+        //Расчитать
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
